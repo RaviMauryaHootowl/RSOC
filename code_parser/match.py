31748dc4c12d0204b6bac1e6481127236a26ab21
RaviMauryaHootowl <ravimaurya027@gmail.com>
2021-12-02 11:51:14 +0530

    Removing ifelif ladder with getattr

--- code_parser/match.py
+++ code_parser/match.py
@@ -11,33 +11,5 @@ def match_command(self, line):
 
 
 def match_pattern(self, line):
-    # print(line)
-    command_type = line.split()[0]
-    if command_type == "SET":
-        self.parse_set(line)
-    elif command_type == "SHOW":
-        self.parse_show(line)
-    elif command_type == "ADD":
-        self.parse_add(line)
-    elif command_type == "SUB":
-        self.parse_sub(line)
-    elif command_type == "MUL":
-        self.parse_mul(line)
-    elif command_type == "DIV":
-        self.parse_div(line)
-    elif command_type == "INR":
-        self.parse_inr(line)
-    elif command_type == "DCR":
-        self.parse_dcr(line)
-    elif command_type == "IFG":
-        self.parse_ifg(line)
-    elif command_type == "IFL":
-        self.parse_ifl(line)
-    elif command_type == "IFE":
-        self.parse_ife(line)
-    elif command_type == "IFN":
-        self.parse_ifn(line)
-    elif command_type == "JUMP":
-        self.parse_jump(line)
-    elif command_type == "SKIP":
-        self.parse_skip(line)
+    command_type = line.split()[0].lower()
+    getattr(self, f"parse_{command_type}")(line)
